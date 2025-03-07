18d3bef9c807a82f8c0b1b4844633c2a49963cdc
Aakriti Kaushik <149955040+aakaru@users.noreply.github.com>
2025-03-08 00:21:01 +0530

    Create main.go

---
+++ go/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/c-bata/go-prompt"
+)
+
+var builtinCommands map[string]func([]string) error
+
+func init() {
+	builtinCommands = map[string]func([]string) error{
+		"cd":   changeDirectory,
+		"pwd":  printWorkingDirectory,
+		"echo": echoCommand,
+		"type": typeCommand,
+		"exit": exitCommand,
+	}
+}
+
+func main() {
+	fmt.Println("GoShell v0.1 - TYpe 'exit' to exit.")
+
+	p := prompt.New(executor,
+		completer,
+		prompt.OptionTitle("GoShell"),
+		prompt.OptionPrefix(shellPrompt()),
+		prompt.OptionLivePrefix(func() (string, bool) {
+			return shellPrompt(), true
+		}),
+	)
+	p.Run()
+
+}
